7f0ff506e15f3106932a9f64180869c53ae5f4bc
becwright226 <becwright226@gmail.com>
2022-02-07 20:09:52 -0500

    Onload fetch kinda working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Event from './components/ticketmaster/ticketApp';
 
 function App() {
   return (
     <div className="App">
-      
+      <Event/>
     </div>
   );
 }
---
+++ src/components/ticketmaster/ticketIndex.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Event from './EventApp';
+
+
+const EventIndex = (props) => {
+  return ( 
+    <div>
+    <Event />
+  </div>
+   );
+}
+ 
+export default EventIndex;
