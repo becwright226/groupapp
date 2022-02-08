9c6624de3f5b00deb957d35aa827e41989d13346
Abby Kelly <akindred4@ivytech.edu>
2022-02-08 12:34:03 -0500

    added geolocation function to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,33 @@ import React, { useState } from 'react';
 
 
 function App() {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
 
-  
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus('Geolocation is not supported by your browser');
+    } else {
+      setStatus('Locating...');
+      navigator.geolocation.getCurrentPosition((position) => {
+        setStatus(null);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, () => {
+        setStatus('Unable to retrieve your location');
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <WeatherIndex />
+      <button onClick={getLocation}>Get Location</button>
+      <h1>Your current coordinates:</h1>
+      <p>{status}</p>
+      {lat && <p>Latitude: {lat}</p>}
+      {lng && <p>Longitude: {lng}</p>}
+      <WeatherIndex lat={lat} lng={lng}/>
     </div>
   );
 }
